67d98291aedc96f588a19e821fa8e3efdfec1592
Juhee Noh <nohjuhee227@gmail.com>
2023-02-14 23:53:47 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// React에서는 뒤에 .js를 생략한다.
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
